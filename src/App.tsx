62e721ac3e7599598ba1fc77b56d4feaeefc9903
Mateus <mateushbsb@gmail.com>
2022-01-24 15:48:39 -0300

    componente content

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from 'react';
 
 import { Button } from './components/Button';
 import { MovieCard } from './components/MovieCard';
+import { Content } from './components/Content';
 
 // import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
 
 import { api } from './services/api';
 
@@ -77,19 +77,8 @@ export function App() {
 
       </nav>
 
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+      <Content movies={movies} selectedGenre={selectedGenre}/>
+      
     </div>
   )
 }
